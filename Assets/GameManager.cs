9e794493f65281dcc083e1f136d4fffe3d5630e0
TichaNicha <79116169+TichaNicha@users.noreply.github.com>
2024-04-29 20:23:12 +1200

    Added nametags

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public static List<int> collectedItems = new List<int>();
     public static float moveSpeed = 3.5f, moveAccuracy = 0.15f;
+    public RectTransform nameTag;
 
+    public Animator playerAnimator;
+
+    void Start()
+    {
+        playerAnimator.SetBool("isWalking", false);
+    }
     
-    public IEnumerator MoveToPoint(Transform myObject, Vector2 point){        
+    public IEnumerator MoveToPoint(Transform myObject, Vector2 point){  
+
+        playerAnimator.SetBool("isWalking", true);
+
         // set direction
         Vector2 positionDifference = point - (Vector2)myObject.position;
         while(positionDifference.magnitude>moveAccuracy){        
@@ -22,6 +34,19 @@ public class GameManager : MonoBehaviour
         if(myObject == FindObjectOfType<ClickManager>().playerTransform){
             FindObjectOfType<ClickManager>().playerWalking = false;
         }
+
+        playerAnimator.SetBool("isWalking", false);
+
         yield return null; // do it over the course of multiple frames
+    }
+
+
+    public void UpdateNameTag(itemData item){
+        // change name
+        nameTag.GetComponentInChildren<TextMeshProUGUI>().text = item.objectName;
+        // change nametag size
+        nameTag.sizeDelta = item.nameTagSize;
+        // move tag
+        nameTag.localPosition= new Vector2(item.nameTagSize.x/2, -0.5f);
     }
 }
--- Assets/itemData.cs
+++ Assets/itemData.cs
@@ -7,4 +7,6 @@ public class itemData : MonoBehaviour
     public int itemID, requiredItemID;    
     public Transform goToPoint;
     public GameObject[] objectsToRemove;
+    public string objectName;
+    public Vector2 nameTagSize = new Vector2(2.5f, 0.5f);
 }
